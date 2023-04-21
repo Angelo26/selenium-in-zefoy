2e85598b4b0e712c993adaf045e6b0b8a42bd5b0
Angelo26 <angelmaden333@gmail.com>
2023-04-22 00:34:04 +0545

    added function to screenshot the code

--- sel.py
+++ sel.py
@@ -1,27 +1,27 @@
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
-import pytesseract 
-import PIL.Image
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+# import pytesseract 
+# import PIL.Image
 
-myconfig = r"--psm 6 --oem 3"
+# myconfig = r"--psm 6 --oem 3"
 
-text = pytesseract.image_to_string(PIL.Image.open("sc.png"), config=myconfig)
+# text = pytesseract.image_to_string(PIL.Image.open("sc.png"), config=myconfig)
  
-print(text)
+# print(text)
 
-# options = Options()
-# options.add_experimental_option("detach", True)
+options = Options()
+options.add_experimental_option("detach", True)
 
-# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# driver.get("https://www.zefoy.com/")
-# driver.implicitly_wait(10)
+driver.get("https://www.zefoy.com/")
+driver.implicitly_wait(10)
 
 
-# capRead = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/img")
-
+capRead = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/img")
+capRead.screenshot("code.png")
 # capWrite = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/div/input")
 # capWrite.clear()
 # capWrite.send_keys("Angel") 
