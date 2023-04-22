3671c3c5b476aacf23a907de74f3ff2502c4aae9
Angelo26 <angelmaden333@gmail.com>
2023-04-22 08:19:33 +0545

    completed

--- sel.py
+++ sel.py
@@ -1,40 +1,93 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import pytesseract 
 import PIL.Image
+import time
 
-options = Options()
-# options.add_argument('headless')
-options.add_experimental_option("detach", True)
+opt = Options()
+# opt.add_argument('headless')
+opt.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=opt)
 
 driver.get("https://www.zefoy.com/")
 driver.implicitly_wait(10)
 
-def enterCode():
-    def saveCode():
-        capRead = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/img")
-        capRead.screenshot("code.png")
+def viewIncreaser():
 
-    def readCode():
+    def enterCode():
+        def saveCode():
+            capRead = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/img")
+            capRead.screenshot("code.png")
+            writeCode()
+
+        def readCode():
+            return pytesseract.image_to_string(PIL.Image.open("code.png"))
+        
+        def writeCode():
+            givenCode = readCode()
+            capWrite = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/div/input")
+            capWrite.clear()
+            capWrite.send_keys(givenCode)
+            clickCapBtn()
+
+        def clickCapBtn():
+            capBtns = driver.find_elements('xpath','/html/body/div[5]/div[2]/form/div/div/div/div/button')
+            for capBtn in capBtns:
+                if bool(capBtns):
+                    capBtn.click()
+                    break
         saveCode()
-        myconfig = r"--psm 6 --oem 3"
-        return pytesseract.image_to_string(PIL.Image.open("code.png"), config=myconfig)
+
+    def addViews():
+        def enterViews():
+            enterCode()
+            viewsBtns = driver.find_elements('xpath', '/html/body/div[6]/div/div[2]/div/div/div[5]/div/button')
+            if (bool(viewsBtns)):
+                for viewsBtn in viewsBtns:
+                    if bool(viewsBtns):
+                        viewsBtn.click()
+                        addUrl()
+                        break
+            else:
+                driver.refresh()
+                print("Captcha Error")
+                enterViews()
+        def addUrl():
+            tiktokUrl = input("Enter the URL here: ")
+            urlWrite = driver.find_element('xpath',"/html/body/div[10]/div/form/div/input")
+            urlWrite.clear()
+            urlWrite.send_keys(tiktokUrl)            
+            submitUrl()
+
+        def submitUrl():
+            submitUrlBtns = driver.find_elements('xpath', '/html/body/div[10]/div/form/div/div/button')
+            for submitUrlBtn in submitUrlBtns:
+                if bool(submitUrlBtns):
+                    submitUrlBtn.click()
+                    counterReady()
+                    break
+
+        def counterReady():
+            getReadyBtns = driver.find_elements('xpath', '/html/body/div[10]/div/div/div[1]/div/form/button')
+            
+            for getReadyBtn in getReadyBtns:
+                if bool(getReadyBtns):
+                    getReadyBtn.click()
+                    time.sleep(120)
+                    submitUrl()
+                    break
+                else:
+                    time.sleep(120)
+                    submitUrl()
+
+        enterViews()
     
-    def writeCode():
-        givenCode = readCode()
-        capWrite = driver.find_element('xpath',"/html/body/div[5]/div[2]/form/div/div/div/input")
-        capWrite.clear()
-        capWrite.send_keys(givenCode)
+    addViews()
 
-    def clickCapBtn(): 
-        writeCode()
-        capBtn = driver.find_element('class name','submit-captcha')
-        capBtn.click()
-
-    clickCapBtn()
-
-enterCode()
+viewIncreaser()
